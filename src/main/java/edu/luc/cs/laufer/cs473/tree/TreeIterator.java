ef6585f043aa7d2f05145416ca9a1f0d6deeedab
Konstantin Läufer <klaeufer@gmail.com>
2016-12-18 18:28:58 -0600

    applied Google Java format
    Signed-off-by: Konstantin Läufer <klaeufer@gmail.com>

--- src/main/java/edu/luc/cs/laufer/cs473/tree/TreeIterator.java
+++ src/main/java/edu/luc/cs/laufer/cs473/tree/TreeIterator.java
@@ -4,37 +4,36 @@ import java.util.Enumeration;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 import java.util.Queue;
-
 import javax.swing.tree.TreeNode;
 
 public class TreeIterator implements Iterator<TreeNode> {
 
-	TreeNode subject;
-	Queue<TreeNode> driver;
+  TreeNode subject;
+  Queue<TreeNode> driver;
 
-	public TreeIterator(final TreeNode subject, final Queue<TreeNode> driver) {
-		this.subject = subject;
-		this.driver = driver;
-		this.driver.add(subject);
-	}
+  public TreeIterator(final TreeNode subject, final Queue<TreeNode> driver) {
+    this.subject = subject;
+    this.driver = driver;
+    this.driver.add(subject);
+  }
 
-	public boolean hasNext() {
-		return !driver.isEmpty();
-	}
+  public boolean hasNext() {
+    return !driver.isEmpty();
+  }
 
-	public TreeNode next() throws NoSuchElementException {
-		System.out.println("driver = " + driver);
-		// remove first element in driver
-		final Iterator<TreeNode> i = driver.iterator();
-		final TreeNode current = i.next();
-		i.remove();
-		for (final Enumeration<?> e = current.children(); e.hasMoreElements(); ) {
-			driver.add((TreeNode) e.nextElement());
-		}
-		return current;
-	}
+  public TreeNode next() throws NoSuchElementException {
+    System.out.println("driver = " + driver);
+    // remove first element in driver
+    final Iterator<TreeNode> i = driver.iterator();
+    final TreeNode current = i.next();
+    i.remove();
+    for (final Enumeration<?> e = current.children(); e.hasMoreElements(); ) {
+      driver.add((TreeNode) e.nextElement());
+    }
+    return current;
+  }
 
-	public void remove() throws UnsupportedOperationException {
-		throw new UnsupportedOperationException();
-	}
+  public void remove() throws UnsupportedOperationException {
+    throw new UnsupportedOperationException();
+  }
 }
--- src/main/java/edu/luc/cs/laufer/cs473/tree/Trees.java
+++ src/main/java/edu/luc/cs/laufer/cs473/tree/Trees.java
@@ -6,56 +6,60 @@ import java.util.Enumeration;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.PriorityQueue;
-
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.TreeNode;
 
 public class Trees {
 
-	public static void main(String[] args) throws java.io.IOException {
+  public static void main(String[] args) throws java.io.IOException {
 
-		final DefaultMutableTreeNode t = new DefaultMutableTreeNode("1");
-		final DefaultMutableTreeNode t2 = new DefaultMutableTreeNode("2");
-		final DefaultMutableTreeNode t4 = new DefaultMutableTreeNode("4");
-		t.add(t2);
-		t.add(new DefaultMutableTreeNode("3"));
-		t.add(t4);
-		t2.add(new DefaultMutableTreeNode("5"));
-		t2.add(new DefaultMutableTreeNode("6"));
-		t4.add(new DefaultMutableTreeNode("7"));
+    final DefaultMutableTreeNode t = new DefaultMutableTreeNode("1");
+    final DefaultMutableTreeNode t2 = new DefaultMutableTreeNode("2");
+    final DefaultMutableTreeNode t4 = new DefaultMutableTreeNode("4");
+    t.add(t2);
+    t.add(new DefaultMutableTreeNode("3"));
+    t.add(t4);
+    t2.add(new DefaultMutableTreeNode("5"));
+    t2.add(new DefaultMutableTreeNode("6"));
+    t4.add(new DefaultMutableTreeNode("7"));
 
-		for (final Iterator<TreeNode> i = new TreeIterator(t,
-				Collections.asLifoQueue(new LinkedList<TreeNode>())); i.hasNext();) {
-			System.out.print(i.next() + " ");
-		}
-		System.out.println();
+    for (final Iterator<TreeNode> i =
+            new TreeIterator(t, Collections.asLifoQueue(new LinkedList<TreeNode>()));
+        i.hasNext();
+        ) {
+      System.out.print(i.next() + " ");
+    }
+    System.out.println();
 
-		for (final Iterator<TreeNode> i = new TreeIterator(t,
-				new LinkedList<TreeNode>()); i.hasNext();) {
-			System.out.print(i.next() + " ");
-		}
-		System.out.println();
+    for (final Iterator<TreeNode> i = new TreeIterator(t, new LinkedList<TreeNode>());
+        i.hasNext();
+        ) {
+      System.out.print(i.next() + " ");
+    }
+    System.out.println();
 
-		final Comparator<TreeNode> bySize = new Comparator<TreeNode>() {
-			public int compare(TreeNode left, TreeNode right) {
-				return size(left) - size(right);
-			}
-		};
-		for (Iterator<TreeNode> i = new TreeIterator(t,
-				new PriorityQueue<TreeNode>(1, bySize)); i.hasNext();) {
-			System.out.print(i.next() + " ");
-		}
-		System.out.println();
-	}
+    final Comparator<TreeNode> bySize =
+        new Comparator<TreeNode>() {
+          public int compare(TreeNode left, TreeNode right) {
+            return size(left) - size(right);
+          }
+        };
+    for (Iterator<TreeNode> i = new TreeIterator(t, new PriorityQueue<TreeNode>(1, bySize));
+        i.hasNext();
+        ) {
+      System.out.print(i.next() + " ");
+    }
+    System.out.println();
+  }
 
-	public static int size(final TreeNode node) {
-		if (node == null) {
-			throw new IllegalArgumentException("size: node is null");
-		}
-		int result = 1;
-		for (final Enumeration<?> e = node.children(); e.hasMoreElements(); ) {
-			result += size((TreeNode) e.nextElement());
-		}
-		return result;
-	}
+  public static int size(final TreeNode node) {
+    if (node == null) {
+      throw new IllegalArgumentException("size: node is null");
+    }
+    int result = 1;
+    for (final Enumeration<?> e = node.children(); e.hasMoreElements(); ) {
+      result += size((TreeNode) e.nextElement());
+    }
+    return result;
+  }
 }
